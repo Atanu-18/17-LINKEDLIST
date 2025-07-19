25a610598903533d8d0c935c2a487b15e5e52080
atanu mandal <atanu2697@gmail.com>
2025-07-20 02:05:07 +0530

    update code

--- LL.java
+++ LL.java
@@ -1,32 +1,31 @@
 class LL {
     Node head;
 
-    class Node{
+    class Node {
         String data;
         Node next;
 
-        //constructor
+        // constructor
         Node(String data) {
             this.data = data;
             this.next = null;
         }
     }
 
-    //add - first, last
-    //first
-    
+    // add - first, last
+    // first
+
     public void addFirst(String data) {
-        Node newNode = new Node(data);
+        Node newNode = new Node(data); // নতুন নোড বানালাম
         if (head == null) {
-            head = newNode;
+            head = newNode; // যদি লিস্ট ফাঁকা থাকে
             return;
         }
-
-        newNode.next = head;
-        head = newNode;
+        newNode.next = head; // নতুন নোডের পরেরটা হবে head
+        head = newNode; // head কে সরিয়ে নিয়ে আসলাম নতুন নোডে
     }
 
-    //last
+    // last
     public void addLast(String data) {
         Node newNode = new Node(data);
         if (head == null) {
@@ -36,40 +35,38 @@ class LL {
 
         Node currNode = head;
         while (currNode.next != null) {
-            currNode = currNode.next;
+            currNode = currNode.next; // একদম শেষে যাই
         }
 
-        currNode.next = newNode;
+        currNode.next = newNode; // শেষে গিয়ে নতুনটা বসাই
     }
 
-    //print
+    // print
     public void printList() {
         if (head == null) {
             System.out.println("list is empty");
             return;
         }
+
         Node currNode = head;
         while (currNode != null) {
             System.out.print(currNode.data + "->");
-            currNode = currNode.next;
+            currNode = currNode.next; // পরের নোডে যাই
         }
 
-        System.out.println("NULL");
+        System.out.println("NULL"); // শেষে পৌঁছালে
     }
 
-
     public static void main(String[] args) {
-        //object
+        // object
         LL list = new LL();
         list.addFirst("a");
         list.addFirst("is");
 
         list.printList();
 
-
         list.addLast("List");
         list.printList();
-
 
         list.addFirst("this");
         list.printList();
